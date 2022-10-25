a1e5f298f23b62dc4f5644f48db8aa839d3a4abe
RinaldiApap <rinaldiapap@gmail.com>
2022-10-25 16:06:22 +0700

    tampilan karir

--- src/views/frontend/Careers.vue
+++ src/views/frontend/Careers.vue
@@ -25,6 +25,39 @@
                 us.</span
               >
             </div>
+            <div v-for="(career, index) in career" :key="index">
+              <div class="row">
+                <div class="joblist">
+                  <section class="toggle">
+                    <h3>{{ career.nama_career }}</h3>
+                    <div class="toggle-content">
+                      <p><strong>Job Description:</strong></p>
+                      <p></p>
+                      <p>
+                        {{ career.desc_career }}
+                      </p>
+                      <p></p>
+
+                      <p><strong>Requirements:</strong></p>
+                      <p></p>
+                      <p>
+                        {{ career.require_career }}
+                      </p>
+                      <p></p>
+
+                      <p>
+                        <a
+                          class="btn btn-primary push-bottom"
+                          href="mailto:recruitment@simetriputra.co.id"
+                          >Apply Now</a
+                        >
+                      </p>
+                    </div>
+                  </section>
+                </div>
+              </div>
+            </div>
+
             <div id="faqs-list">
               <div class="row align-items-stretch grid-border clearfix"></div>
             </div>
@@ -40,5 +73,82 @@
   <!-- #content end -->
 </template>
 <script>
-export default {};
+import axios from "axios";
+import { onMounted, ref } from "vue";
+export default {
+  setup() {
+    let career = ref([]);
+
+    onMounted(() => {
+      axios
+        .get("http://127.0.0.1:8000/api/show_career")
+        .then((result) => {
+          career.value = result.data;
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+    });
+    return {
+      career,
+    };
+  },
+  methods: {
+    toggleOpen: function (index) {
+      this.career = this.career.map((career, index) => {
+        if (index === index) {
+          career.open = !career.open;
+        }
+
+        return career;
+      });
+    },
+  },
+};
 </script>
+<style>
+.joblist {
+  display: block;
+  width: 100%;
+  max-width: 800px;
+  margin: 15px auto;
+  padding: 15px;
+  border-radius: 8px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  background-color: #fff;
+}
+
+.toggle {
+  position: relative;
+  color: #3c3c3c;
+  font-size: 20px;
+  height: 20px;
+  transition: all 0.4s linear;
+}
+
+.toggle::after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  right: 0px;
+  transform: translateY(-50%);
+  rotate: (0deg);
+
+  width: 35px;
+  margin: 8px;
+  height: 35px;
+  background-image: url("../src/assets/style/images/bluearrow.png");
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+
+.toggle-content {
+  color: #3c3c3c;
+  font-size: 18px;
+  opacity: 0;
+  max-height: 0px;
+  overflow-y: hidden;
+  transition: all 0.4s ease-out;
+}
+</style>
